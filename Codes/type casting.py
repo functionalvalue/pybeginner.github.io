cd729d7dccd51cdea80dbfc8cb1ecef6dc00b0d0
Functional Value <awladstd@gmail.com>
2021-08-18 13:34:27 +0600

    Type Casting File Added

---
+++ Codes/type casting.py
@@ -0,0 +1,32 @@
+# Integer to Float
+# a = 5
+# print(a)
+# print(type(a))
+# b = float(a)
+# print(b)
+# print(type(b))
+
+# Float int
+
+# a = 7.654521
+# print(a)
+# print(type(a))
+#
+# b = int(a)
+# print(b)
+# print(type(b))
+
+#Integer and String
+
+a = 5
+print(a)
+b = str(a)
+print(type(b))
+print(b)
+
+s = '4.6'
+print(s)
+print(type(s))
+i = int(s)
+print(i)
+print(type(i))
